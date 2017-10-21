92595a96aadef2c47cbb5b70ecaf624ecc94a6e5
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-21 11:37:54 +0200

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,1 +1,24 @@
 
+plot4 <- function(){
+  combustion <-grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+  coal <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+  coalCombustion <- (coal & combustion)
+  ccSCC <- SCC[coalCombustion,]$SCC
+  SCC <- NULL
+  coal <- NULL
+  combustion <- NULL
+  
+  NEI$year <- as.factor(NEI$year)
+  
+  ccNEI <- NEI[NEI$SCC %in% ccSCC,]
+  ccNEI <- ccNEI[,c("year", "Emissions")]
+  
+  s <- aggregate(Emissions~year,ccNEI,FUN=sum)
+  ccNEI <- NULL
+  
+  plot4 <- ggplot(s,aes(year,Emissions))+geom_bar(stat="identity")+xlab("Year")+ylab("PM2.5 (tons)")+ggtitle("Coal Combustion-related Emissions in US")
+  
+  print(plot4)
+}
+
+plot4()
