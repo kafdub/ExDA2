afdcda0d4caca68e43a4afb68e125fbb87541c0b
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-21 11:35:19 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -5,7 +5,6 @@ plot2 <- function(){
   freqPerYear <- aggregate(Emissions~year,subNEI, FUN=sum)
   subNEI <- NULL
   with(freqPerYear,barplot(Emissions, names = year, xlab = "Year", ylab = "PM2.5 (tons)", main = "PM2.5 Total Emissions in Baltimore City, Marland"))
-  
 }
 
 plot2()
