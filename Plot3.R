d354ebf833ebf2801d228676cde93d73bf848724
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-21 11:36:22 +0200

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,1 +1,18 @@
 
+plot3 <- function(){
+  library("ggplot2")
+  subNEI <- NEI[NEI$fips=="24510", c("Emissions", "year", "type")]
+  
+  subNEI$year <- as.factor(subNEI$year)
+  subNEI$type <- as.factor(subNEI$type)
+  
+  freqPerYear <- aggregate(Emissions~year + type, subNEI, FUN=sum)
+  
+  subNEI <- NULL
+  
+  plot3 <- ggplot(freqPerYear, aes(year, Emissions, fill=type))+ guides(fill=FALSE)+geom_bar(stat="identity")+facet_grid(.~type) +xlab("Year") + ylab("PM2.5 (tons)") + ggtitle("PM2.5 Total Emissions in Baltimore City, Marland (by Type)")
+  
+  print(plot3)
+}
+
+plot3()
