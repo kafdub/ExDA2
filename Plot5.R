d2b18e8edc0c79dc4dccf3d97930f5063198fd6e
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-21 11:38:46 +0200

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,1 +1,19 @@
+plot5 <- function(){
+  vehicles <-grepl("vehicle", SCC$SCC.Level.Two, ignore.case = TRUE)
+  vSCC <- SCC[vehicles,]$SCC
+  
+  NEI$year <- as.factor(NEI$year)
+  subNEI <- NEI[NEI$fips=="24510", c("SCC", "Emissions", "year")]
+  
+  vNEI <- subNEI[subNEI$SCC %in% vSCC, ]
+  vNEI <- vNEI[,c("year", "Emissions")]
+  
+  s <- aggregate(Emissions~year, vNEI, FUN=sum)
+  vNEI <- NULL
+  
+  plot5 <- ggplot(s,aes(year,Emissions))+geom_bar(stat="identity")+xlab("Year")+ylab("PM2.5 (tons)")+ggtitle("Emissions from Vehicles in Baltimore City, Maryland")
+  
+  print(plot5)
+}
 
+plot5()
