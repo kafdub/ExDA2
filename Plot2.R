a00c7fbecc58e30a7e0e4afb4a580181c6b13aa8
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-21 11:34:52 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,1 +1,11 @@
 
+plot2 <- function(){
+  subNEI <- NEI[NEI$fips=="24510", c("Emissions", "year")]
+  subNEI$year=as.factor(subNEI$year)
+  freqPerYear <- aggregate(Emissions~year,subNEI, FUN=sum)
+  subNEI <- NULL
+  with(freqPerYear,barplot(Emissions, names = year, xlab = "Year", ylab = "PM2.5 (tons)", main = "PM2.5 Total Emissions in Baltimore City, Marland"))
+  
+}
+
+plot2()
